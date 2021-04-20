5e7c1b5607faf90612b08ae8e9ee2f95020dbdeb
yashwalankar <yashw5995@gmail.com>
2021-04-20 00:09:46 -0400

    marketing manager landing page

--- MarketingLandingPage.Designer.cs
+++ MarketingLandingPage.Designer.cs
@@ -29,12 +29,118 @@ namespace Air3550
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.marketingManger_welcome_label = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.selectedFlight_label = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // marketingManger_welcome_label
+            // 
+            this.marketingManger_welcome_label.AutoSize = true;
+            this.marketingManger_welcome_label.Location = new System.Drawing.Point(30, 19);
+            this.marketingManger_welcome_label.Name = "marketingManger_welcome_label";
+            this.marketingManger_welcome_label.Size = new System.Drawing.Size(144, 13);
+            this.marketingManger_welcome_label.TabIndex = 0;
+            this.marketingManger_welcome_label.Text = "Welcome, Marketing Manger";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(33, 67);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(411, 340);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // selectedFlight_label
+            // 
+            this.selectedFlight_label.AutoSize = true;
+            this.selectedFlight_label.Location = new System.Drawing.Point(521, 86);
+            this.selectedFlight_label.Name = "selectedFlight_label";
+            this.selectedFlight_label.Size = new System.Drawing.Size(80, 13);
+            this.selectedFlight_label.TabIndex = 2;
+            this.selectedFlight_label.Text = "Selected Flight:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(617, 173);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(521, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Current Plane:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(521, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Current Capacity:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(521, 211);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Current Capacity:";
+            
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(521, 176);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Set Plane:";
+            // 
+            // MarketingLandingPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.selectedFlight_label);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.marketingManger_welcome_label);
+            this.Name = "MarketingLandingPage";
             this.Text = "MarketingLandingPage";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label marketingManger_welcome_label;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label selectedFlight_label;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
