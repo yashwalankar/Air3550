ef0b8de5b9295277f97902d23e4e804c07f3f554
yashwalankar <yashw5995@gmail.com>
2021-04-20 00:48:50 -0400

    Added Plane model and capacity Table with values

--- MarketingLandingPage.Designer.cs
+++ MarketingLandingPage.Designer.cs
@@ -37,6 +37,7 @@ namespace Air3550
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -60,7 +61,7 @@ namespace Air3550
             // selectedFlight_label
             // 
             this.selectedFlight_label.AutoSize = true;
-            this.selectedFlight_label.Location = new System.Drawing.Point(521, 86);
+            this.selectedFlight_label.Location = new System.Drawing.Point(517, 86);
             this.selectedFlight_label.Name = "selectedFlight_label";
             this.selectedFlight_label.Size = new System.Drawing.Size(80, 13);
             this.selectedFlight_label.TabIndex = 2;
@@ -77,7 +78,7 @@ namespace Air3550
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(521, 109);
+            this.label1.Location = new System.Drawing.Point(523, 109);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(74, 13);
             this.label1.TabIndex = 4;
@@ -86,7 +87,7 @@ namespace Air3550
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(521, 132);
+            this.label2.Location = new System.Drawing.Point(509, 132);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(88, 13);
             this.label2.TabIndex = 5;
@@ -95,27 +96,37 @@ namespace Air3550
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(521, 211);
+            this.label3.Location = new System.Drawing.Point(521, 210);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(88, 13);
+            this.label3.Size = new System.Drawing.Size(76, 13);
             this.label3.TabIndex = 7;
-            this.label3.Text = "Current Capacity:";
-            
+            this.label3.Text = "New Capacity:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(521, 176);
+            this.label4.Location = new System.Drawing.Point(541, 176);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(56, 13);
             this.label4.TabIndex = 6;
             this.label4.Text = "Set Plane:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(552, 278);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Update Plane";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // MarketingLandingPage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label2);
@@ -142,5 +153,6 @@ namespace Air3550
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
     }
 }
--- MarketingLandingPage.cs
+++ MarketingLandingPage.cs
@@ -16,5 +16,10 @@ namespace Air3550
         {
             InitializeComponent();
         }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
