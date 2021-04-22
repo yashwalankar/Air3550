7373068b50050bc81d6d3f2062a249f156f8db81
yashwalankar <yashw5995@gmail.com>
2021-04-21 21:20:22 -0400

    marketing manager page completed, new plane model updates in database

--- MarketingLandingPage.cs
+++ MarketingLandingPage.cs
@@ -7,7 +7,9 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 using System.Windows.Forms;
+using DataGrid = System.Windows.Controls.DataGrid;
 
 namespace Air3550
 {
@@ -19,12 +21,38 @@ namespace Air3550
             FormDatabaseHelper.FillPlaneModelsBox(planesModels_comboBox);
         }
 
-        
-        
+
+        private void marketing_datagrid_SelectionChanged(object sender, EventArgs e)
+        {
+            
+                DataGridViewRow currentRow = marketing_datagrid.SelectedRows[0];
+                String flightId = "";
+                String flight_string = "";
+                String plane_type = "";
+                String maxCapacity = "";
+                if (currentRow != null)
+                {
+                    flightId = currentRow.Cells[0].Value.ToString();
+                    flight_string = currentRow.Cells[1].Value.ToString() +
+                                    "--> " + currentRow.Cells[2].Value.ToString();
+
+                    plane_type = currentRow.Cells[4].Value.ToString();
+                    maxCapacity = currentRow.Cells[5].Value.ToString();
+
+                }
+
+                selFlightValue_textbox.Text = flight_string;
+                currPlane_textbox.Text = plane_type;
+                curr_maxCapacity_textbox.Text = maxCapacity;
+                selFlightId_textbox.Text = flightId;
+            
+        }
+
 
         private void planesModels_comboBox_SelectedIndexChanged(object sender, EventArgs e)
         {
-            FormDatabaseHelper.updateCapacityBox(planesModels_comboBox.SelectedItem.ToString() , newCapacityValue_label);
+            newCapacityValue_textBox.Text = FormDatabaseHelper.getPlaneCapacity(planesModels_comboBox.SelectedItem.ToString()).ToString();
+            //FormDatabaseHelper.updateCapacityBox(planesModels_comboBox.SelectedItem.ToString() , newCapacityValue_label);
         }
 
         private void logout_button_Click(object sender, EventArgs e)
@@ -33,6 +61,36 @@ namespace Air3550
             this.Dispose();
             Login s = new Login();
             s.Visible = true;
+        }
+
+        private void MarketingLandingPage_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'air3550DBDataSet.Flights' table. You can move, or remove it, as needed.
+            this.flightsTableAdapter.Fill(this.air3550DBDataSet.Flights);
+
+        }
+
+        private void updatePlane_btn_Click(object sender, EventArgs e)
+        {
+            String planeModel = "";
+            String capacityStr =  newCapacityValue_textBox.Text;
+
+            if (!String.IsNullOrEmpty(capacityStr))
+            {
+                planeModel = planesModels_comboBox.SelectedItem.ToString();
+                int flightId = Int32.Parse(selFlightId_textbox.Text);
+                int capacity = Int32.Parse(capacityStr);
+                
+
+                //updates in database/flights table
+                FormDatabaseHelper.updatePlaneInFlightsTable(flightId, planeModel, capacity);
+
+                //update in the data grid view
+                marketing_datagrid.SelectedRows[0].Cells[4].Value = planeModel;
+                marketing_datagrid.SelectedRows[0].Cells[5].Value = capacity;
+
+            }
+            
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace Air3550
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login());
+            Application.Run(new MarketingLandingPage());
         }
     }
 }
